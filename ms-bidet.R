b23f5e690909d429e7d12226fd33d1d02886cf19
Adam Orr <adamjorr@gmail.com>
2017-09-21 17:15:45 -0700

    more work, add test data

--- ms-bidet.R
+++ ms-bidet.R
@@ -13,31 +13,30 @@ if (length(arguments) == 0){
   arguments <- "."
 }
 
-files <- list.files(arguments[1])
+files <- list.files(arguments[1], full.names = TRUE)
 dirs <- files[file_test('-d', files)]
 
 if (length(dirs) == 0){
   stop(paste0("No directories in the given directory \"",arguments,"\""))
 }
 
-writeLines(dirs)
-
 # > cdn <- read_csv("~/Dropbox (ASU)/Lake_Doug_Valley_Fever/CDN.csv")
 # > cdn[1,]
 
 load_csvs <- function(foldername){
-  list.files(foldername, pattern = "*.csv") %>%
+  list.files(foldername, full.names = TRUE, pattern = "*.csv") %>%
     map(~read_csv(.)) %>%
     bind_rows() %>%
     select(1) %>% #take just first column
     rename_at(1, ~"mz") %>% #rename first column mz
     group_by(mz) %>%
     tally() %>%
-    rename_at(2, ~foldername) #rename col with tally as name of folder
+    rename_at(2, ~basename(foldername)) #rename col with tally as name of folder
 }
 
 df <- map(dirs, ~load_csvs(.)) %>%
-  join_all(by = "mz", type = "full")
+  join_all(by = "mz", type = "full") %>%
+  mutate_all(~coalesce(., 0L))
 
 res <- fisher.test(df)
 
