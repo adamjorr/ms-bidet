1a8c0f790498c376523c48459625e274b960cab4
Adam Orr <adamjorr@gmail.com>
2017-09-22 14:46:24 -0700

    change a few things

--- ms-bidet.R
+++ ms-bidet.R
@@ -23,20 +23,32 @@ if (length(dirs) == 0){
 # > cdn <- read_csv("~/Dropbox (ASU)/Lake_Doug_Valley_Fever/CDN.csv")
 # > cdn[1,]
 
+mut_add_contingency <- function(df, fname){
+  fname <- enquo(fname)
+  sname <- quo_name(fname)
+  mutate(df, !!sname := 1)
+}
+
+load_sample <- function(filename){
+  sam <- tools::file_path_sans_ext(basename(filename))
+  print(sam)
+  read_csv(filename) %>%
+    select(1) %>% #take just first column
+    rename_at(1,~"mz") %>% # rename column to mz
+    mutate(!!sam := 1, foo = "bar") #!! and := mean use the value of filename as the new variable name
+}
+
 load_csvs <- function(foldername){
   list.files(foldername, full.names = TRUE, pattern = "*.csv") %>%
-    map(~read_csv(.)) %>%
-    bind_rows() %>%
-    select(1) %>% #take just first column
-    rename_at(1, ~"mz") %>% #rename first column mz
-    group_by(mz) %>%
-    tally() %>%
-    rename_at(2, ~basename(foldername)) #rename col with tally as name of folder
+    map(~load_sample(.)) %>%
+    join_all(by = "mz", type = "full")
 }
+
 
 df <- map(dirs, ~load_csvs(.)) %>%
   join_all(by = "mz", type = "full") %>%
-  mutate_all(~coalesce(., 0L))
+  mutate_all(~coalesce(., 0L)) %>%
+  select()
 
 res <- fisher.test(df)
 
