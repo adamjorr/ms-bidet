63c0f59bc33c1a4d5d4dff071056926efcf5f0dc
Adam Orr <adamjorr@gmail.com>
2017-09-25 16:15:52 -0700

    prefilter to remove NAs, add comments

--- ms-bidet.R
+++ ms-bidet.R
@@ -19,6 +19,7 @@ get_files <- function(dir){
   list.files(dir, full.names = TRUE)
 }
 
+# ---- get list of files and save directories and groups ----
 files <- get_files(arguments[1])
 dirs <- files[file_test('-d', files)]
 grps <- tools::file_path_sans_ext(basename(dirs))
@@ -27,79 +28,93 @@ if (length(dirs) == 0){
   stop(paste0("No directories in the given directory \"",arguments,"\""))
 }
 
-# ---- long form ----
+# ---- long form data manipulation ----
+#get a table from each file and do some filtering
 load_sample <- function(filename){
   sam <- tools::file_path_sans_ext(basename(filename))
-  read_csv(filename) %>%
+  suppressMessages(read_csv(filename)) %>%
     select(1) %>% #take just first column
     rename_at(1,~"mz") %>% # rename column to mz
-    distinct() %>%
+    distinct() %>% #get rid of duplicated rows
+    na.omit() %>% #get rid of any rows with NAs
     mutate(sam = sam) #!! and := mean use the value of filename as the new variable name
 }
 
+#load all the csvs in a folder as one group
 load_csvs <- function(foldername){
   grpname <- tools::file_path_sans_ext(basename(foldername))
-  list.files(foldername, full.names = TRUE, pattern = "*.csv") %>%
-    map(~load_sample(.)) %>%
-    bind_rows() %>%
-    mutate(grp = grpname) %>%
-    mutate(val = 1L)
+  list.files(foldername, full.names = TRUE, pattern = "*.csv") %>% #load all csvs
+    map(~load_sample(.)) %>% #load and prefilter them
+    bind_rows() %>% #concatenate them
+    mutate(grp = grpname) %>% #add the groupname as a variable
+    mutate(val = 1L) #add column indicating presence of m/z peak
 }
 
+#add a val = 0 row if a sample doesn't have a peak that's present in another sample
 add_zeros <- function(df, charges, samp){
-  charges %>%
-    left_join(filter(df, sam == samp), by = "mz") %>%
-    mutate(val = coalesce(.$val, 0L)) %>%
-    mutate(sam = na.omit(.$sam)[1]) %>%
-    mutate(grp = na.omit(.$grp)[1])
+  charges %>% #charges should be all possible m/z peaks
+    left_join(filter(df, sam == samp), by = "mz") %>% #left join with one sample to leave NAs where peak is absent
+    mutate(val = coalesce(.$val, 0L)) %>% #change the NAs to 0's
+    mutate(sam = na.omit(.$sam)[1]) %>% #change the samples to the first non-NA value in the group var. should be equal to samp.
+    mutate(grp = na.omit(.$grp)[1]) #change the group values to the first non-NA value in group variable.
 }
 
+#look in the long form data (df) to calculate contingency table
 build_contingency <- function(mzd, df){
   m <- df %>%
-    filter(mz == mzd) %>%
-    select(-mz, -val)
+    filter(mz == mzd) %>% #only take correct peak
+    select(-mz, -val) #only want counts in matrix
   as.matrix(m)
 }
 
 control_group <- "control"
 
+#big table of raw data
 df <- map(dirs, ~load_csvs(.)) %>%
   bind_rows()
 
+#table just from control group
 controldf <- df %>%
   filter(grp == control_group)
 
+#remove rows in data that aren't in control group
 df <- df %>%
   filter(mz %in% controldf$mz) %>%
   filter(grp != control_group)
 
+#desired peaks possible in data
 charges <- select(df, mz) %>%
   distinct()
 
-tables <- map(unique(df$sam), ~add_zeros(df, charges, .)) %>%
-  bind_rows() %>%
+#create table with all peaks and whether they are present for each sample
+tables <- map(unique(df$sam), ~add_zeros(df, charges, .)) %>% #for every sample, add rows for peaks that are missing
+  bind_rows() %>% #put them in one big table
   group_by(mz, grp) %>%
-  summarise(present = sum(val == 1), absent = sum(val == 0)) %>%
-  ungroup()
+  summarise(present = sum(val == 1), absent = sum(val == 0)) %>% #count present and absent by peak and group
+  ungroup() #ungroup table
 
+#transpose so each group gets a column
 present <- tables %>%
   select(-absent) %>%
-  spread(grp, present) %>%
+  spread(grp, present) %>% #spread so each group is a column, values in table are counts of samples in group that have peak
   mutate(val = "present")
   
 absent <- tables %>%
   select(-present) %>%
-  spread(grp, absent) %>%
+  spread(grp, absent) %>% #spread so each group is a column, values in table are counts of samples in group that don't have peak
   mutate(val = "absent")
 
+#combine present + absent into one big table
 full_contingencies <- bind_rows(present, absent)
 
+#do a fisher test for each peak
 pvals <- unlist(map(charges$mz, ~fisher.test(build_contingency(.,full_contingencies))$p))
-adjusted <- p.adjust(pvals)
+adjusted <- p.adjust(pvals) #adjust p-values for multiple tests
 
 out <- charges %>%
   mutate( p = pvals ) %>%
-  mutate( q = adjusted)
+  mutate( q = adjusted) %>%
+  arrange(q)
 
 writeLines(format_csv(out), stdout())
 
